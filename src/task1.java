0399a1546cecb99906f314f15a701952cda3087b
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-06 11:07:11 +0400

    taske3 added

--- src/task1.java
+++ src/task1.java
@@ -3,7 +3,8 @@ public class task1 {
         int[] nums ={6, 1, 2, 3};
         if (nums[0]==6 || nums[nums.length]==6) {
             System.out.println("true");
+        } else{
+            System.out.println("false");
         }
-
     }
 }
---
+++ src/task3.java
@@ -0,0 +1,8 @@
+public class task3 {
+    public static void main(String[] args){
+      int[] nums = {1, 2, 3};
+      for(int i = nums.length-1; i == 0; i--) {
+          System.out.println(nums[i]);
+      }
+    }
+}
