be0376d19b4ad925604fbe2c3fc57b4081c21c6e
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-06 10:37:37 +0400

    task1 added

---
+++ src/task1.java
@@ -0,0 +1,9 @@
+public class task1 {
+    public static void main(String[] args){
+        int[] nums ={6, 1, 2, 3};
+        if (nums[0]==6 || nums[nums.length]==6) {
+            System.out.println("true");
+        }
+
+    }
+}
