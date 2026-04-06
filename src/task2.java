43311b1d384b5a88eb20cf55ac3d7435ea053f9d
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-06 10:47:40 +0400

    taske2 added

---
+++ src/task2.java
@@ -0,0 +1,11 @@
+public class task2 {
+    public static void main(String[] args){
+        int[] a = {1, 2, 3};
+        int[] b = {7, 3};
+        if(a[0] == b[0] || a[a.length-1] == b[b.length-1]){
+            System.out.println("true");
+        } else{
+            System.out.println("false");
+        }
+    }
+}
